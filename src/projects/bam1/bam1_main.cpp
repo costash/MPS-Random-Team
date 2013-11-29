d7f8a80fdf06d10867c5fa2f6d153a815b8196ec
Tudorica Constantin-Alexandru <tudalex@gmail.com>
2013-11-29 14:22:00 -0800

    Modified BAM
    Modified the BAM skeleton.

--- src/projects/bam1/bam1_main.cpp
+++ src/projects/bam1/bam1_main.cpp
@@ -13,6 +13,25 @@
 //===========================================================================
 //===========================================================================
 
+void do_magic(int intHeight, int intWidth, BYTE **pDataMatrixGrayscale, BYTE **pDataMatrixConfidence, KImage* pImageBinary) {
+	//Apply a threshold at half the grayscale range (0x00 is Full-Black, 0xFF is Full-White, 0x80 is the Middle-Gray)
+    for (int y = intHeight - 1; y >= 0; y--)
+        for (int x = intWidth - 1; x >= 0; x--)
+        {
+            //You may use this instead of the line below: 
+            //    BYTE PixelAtXY = pImageGrayscale->Get8BPPPixel(x, y)
+            BYTE &PixelAtXY = pDataMatrixGrayscale[y][x];
+            if (PixelAtXY < 0x80)
+                //...if closer to black, set to black
+                pImageBinary->Put1BPPPixel(x, y, false);
+            else
+                //...if closer to white, set to white
+                pImageBinary->Put1BPPPixel(x, y, true);
+        }
+}
+
+
+
 //===========================================================================
 //===========================================================================
 int _tmain(int argc, _TCHAR* argv[])
@@ -43,12 +62,15 @@ int _tmain(int argc, _TCHAR* argv[])
     
     //Apply a Gaussian Blur with small radius to remove obvious noise
     pImage->GaussianBlur(0.5);
-    _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_blurred.TIF"), argv[0]);
-    pImage->SaveAs(strNewFileName, SAVE_TIFF_LZW);
-
+	if (_DEBUG) {
+		_stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_blurred.TIF"), argv[0]);
+		pImage->SaveAs(strNewFileName, SAVE_TIFF_LZW);
+	}
     //Convert to grayscale
     KImage *pImageGrayscale = pImage->ConvertToGrayscale();
-    //Don't forget to delete the original, now useless image
+    
+	
+	//Don't forget to delete the original, now useless image
     delete pImage;
 
     //Verify conversion success...
@@ -57,12 +79,18 @@ int _tmain(int argc, _TCHAR* argv[])
         _tprintf(_T("Conversion to grayscale was not successfull!"));
         return -2;
     }
-    //... and save grayscale image
-    _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_grayscale.TIF"), argv[0]);
-    pImageGrayscale->SaveAs(strNewFileName, SAVE_TIFF_LZW);
-    
+	KImage *pImageConfidence = new KImage(*pImageGrayscale);
+
+	
+    if (_DEBUG) {
+		//... and save grayscale image
+		_stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s_grayscale.TIF"), argv[0]);
+		pImageGrayscale->SaveAs(strNewFileName, SAVE_TIFF_LZW);
+	}
+
     //Request direct access to image pixels in raw format
     BYTE **pDataMatrixGrayscale = NULL;
+	BYTE **pDataMatrixConfidence = NULL;
     if (pImageGrayscale->BeginDirectAccess() && (pDataMatrixGrayscale = pImageGrayscale->GetDataMatrix()) != NULL)
     {
         //If direct access is obtained get image attributes and start processing pixels
@@ -71,25 +99,13 @@ int _tmain(int argc, _TCHAR* argv[])
 
         //Create binary image
         KImage *pImageBinary = new KImage(intWidth, intHeight, 1);
-        if (pImageBinary->BeginDirectAccess())
+		if (pImageBinary->BeginDirectAccess() && pImageConfidence->BeginDirectAccess() && (pDataMatrixConfidence = pImageConfidence->GetDataMatrix()) != NULL)
         {
-            //Apply a threshold at half the grayscale range (0x00 is Full-Black, 0xFF is Full-White, 0x80 is the Middle-Gray)
-            for (int y = intHeight - 1; y >= 0; y--)
-                for (int x = intWidth - 1; x >= 0; x--)
-                {
-                    //You may use this instead of the line below: 
-                    //    BYTE PixelAtXY = pImageGrayscale->Get8BPPPixel(x, y)
-                    BYTE &PixelAtXY = pDataMatrixGrayscale[y][x];
-                    if (PixelAtXY < 0x80)
-                        //...if closer to black, set to black
-                        pImageBinary->Put1BPPPixel(x, y, false);
-                    else
-                        //...if closer to white, set to white
-                        pImageBinary->Put1BPPPixel(x, y, true);
-                }
+			do_magic(intHeight, intWidth, pDataMatrixGrayscale, pDataMatrixConfidence, pImageBinary);
 
             //Close direct access
             pImageBinary->EndDirectAccess();
+			pImageConfidence->EndDirectAccess();
             
             //Save binarized image
             _stprintf_s(strNewFileName, sizeof(strNewFileName) / sizeof(TCHAR), _T("%s"), argv[2]);
@@ -97,11 +113,12 @@ int _tmain(int argc, _TCHAR* argv[])
 
             //Don't forget to delete the binary image
             delete pImageBinary;
+			delete pImageConfidence;
         }
         else
         {
-            _tprintf(_T("Unable to obtain direct access in binary image!"));
-            return -30;
+            _tprintf(_T("Unable to obtain direct access in binary image!")); //Nu am idee de alta eroare
+            return -4;
         }
 
         //Close direct access
@@ -109,8 +126,8 @@ int _tmain(int argc, _TCHAR* argv[])
     }
     else
     {
-        _tprintf(_T("Unable to obtain direct access in grayscale image!"));
-        return -40;
+        _tprintf(_T("Unable to obtain direct access in grayscale image!")); //Nu am idee de alta eroare
+        return -4;
     }
 
     //Don't forget to delete the grayscale image
