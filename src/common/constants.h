a58e8571b0c55d8b4f34a284e6142f247c1daa47
Constantin Șerban-Rădoi <costashsrc@gmail.com>
2013-12-02 12:45:57 +0200

    Fix BAM exit codes.

--- src/common/constants.h
+++ src/common/constants.h
@@ -18,10 +18,10 @@
 struct BAM_EXIT_CODE {
     enum values {
         SUCCESS = 0,			// Actually anything >= 0 is SUCCESS
-        READ_ERR = 1,			// Something wrong happened at reading image
-        INPUT_IMAGE_ERR = 2,	// Something wrong with the input image
-        WRITE_ERR = 3,			// Something wrong happened at writing output
-        MEMORY_ERR = 4			// Not enough memory or memory fragmentation
+        READ_ERR = -1,			// Something wrong happened at reading image
+        INPUT_IMAGE_ERR = -2,	// Something wrong with the input image
+        WRITE_ERR = -3,			// Something wrong happened at writing output
+        MEMORY_ERR = -4			// Not enough memory or memory fragmentation
     };
 };
 
